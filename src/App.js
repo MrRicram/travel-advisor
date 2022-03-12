85efa463563d3a79ad7aa459c4507cfc096651d8
Ricardo Ramos <rf.ramos95@gmail.com>
2022-03-12 16:21:41 +0000

    Initialize Components and add them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import Header from './Components/Header/Header';
+import List from './Components/List/List';
+import Map from './Components/Map/Map';
 import React from 'react';
 
 const App = () => {
     return (
-        <div>
-        <h1>Hello, World!</h1>
-        </div>
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
+        </>
     );
 }
 
